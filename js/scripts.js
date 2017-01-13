5170bf4ad7a5bc1ebb1cc8e2765edce9c528add4
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-01-13 15:03:34 -0800

    Reduce redundant code

--- js/scripts.js
+++ js/scripts.js
@@ -7,40 +7,35 @@ var userLocation = 0;
 var id = 0;
 
 var questionLocation = function(){
+  $("#location").fadeOut();
+  $("#question1").fadeIn();
   if (userLocation === "seattle") {
     // alert("Seattle");
-    $("#location").fadeOut();
-    $("#question1").fadeIn();
     $("#seattle").show();
   } else if (userLocation === "portland") {
     // alert("Portland");
-    $("#location").fadeOut();
-    $("#question1").fadeIn();
     $("#portland").show();
   } else {
     // alert("Philadelphia")
-    $("#location").fadeOut();
-    $("#question1").fadeIn();
     $("#philadelphia").fadeIn();;
   };
 };
 
 var question1 = function() {
   $(".def-pop-box").hide();
+  $("#def-pop-container").fadeIn();
+  $("#question1").fadeOut();
   if (frontVsBack === "frontend") {
-    $("#def-pop-container").fadeIn();
     $("#frontend-def").fadeIn();
-    $("#question1").fadeOut();
     // $("#question2").fadeIn();
   } else {
-    $("#def-pop-container").fadeIn();
     $("#backend-def").fadeIn();
-    $("#question1").fadeOut();
     // $("#question2").fadeIn();
   };
 };
 
 var question2 = function() {
+  $("#question2").fadeOut();
   if (company === "large") {
     $("#question3").fadeIn();
   } else {
@@ -49,34 +44,30 @@ var question2 = function() {
 }
 
 var question3 = function() {
+  $(".question").fadeOut();
+  $("#result-container").fadeIn();
   if (android === "android") {
-    $(".question").fadeOut();
-    $("#result-container").fadeIn();
     $("#java").fadeIn();
   } else {
-    $(".question").fadeOut();
-    $("#result-container").fadeIn();
     $("#c-sharp").fadeIn();
   };
 };
 
 var question4 = function() {
+  $(".question").fadeOut();
+  $("#result-container").fadeIn();
   if (rubyPhp === "ruby") {
-    $(".question").fadeOut();
-    $("#result-container").fadeIn();
     $("#ruby").fadeIn();
   } else {
-    $(".question").fadeOut();
-    $("#result-container").fadeIn();
     $("#php").fadeIn();
   };
 };
 
 var buttonClick = function() {
   id = event.target.id;
+  $("#def-pop-container").fadeOut();
+  $(".def-pop-box").fadeOut();
   if (id == "front-back-yes") {
-    $("#def-pop-container").fadeOut();
-    $(".def-pop-box").fadeOut();
     $("#question1").fadeOut();
     if ($("#frontend-def").is(":visible")){
       $("#result-container").fadeIn();
@@ -85,8 +76,6 @@ var buttonClick = function() {
       $("#question2").fadeIn();
     };
   } else {
-    $("#def-pop-container").fadeOut();
-    $(".def-pop-box").fadeOut();
     $("#question1").fadeIn();
     // alert("Front-Back-No");
     // $("#def-pop-container").fadeIn();
